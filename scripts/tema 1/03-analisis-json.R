1bb4ac21052afdb303986f280ab5f6980ee97862
Marcos Londero <marlondero@gmail.com>
2018-11-24 17:06:15 -0300

    lectura de archivos

---
+++ scripts/tema 1/03-analisis-json.R
@@ -0,0 +1,13 @@
+install.packages("jsonlite")
+library(jsonlite)
+install.packages("curl")
+library(curl)
+
+dat.1 <- fromJSON("../data/tema 1/students.json")
+dat.2 <- fromJSON("../data/tema 1/student-courses.json")
+url <- "http://www.floatrates.com/daily/usd.json"
+currencies <- fromJSON(url) 
+currencies$ars$rate
+
+curren <- lapply(currencies[[]], as.data.frame(currencies[[]]))
+View(curren)
